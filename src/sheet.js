7efd918866a4c02ed2a9da05af52be7b6e50192a
Edwin Tay <edwin.r.tay@gmail.com>
2015-11-04 21:42:22 +1100

    Sheet now draws a rectangle.

--- src/sheet.js
+++ src/sheet.js
@@ -1,5 +1,11 @@
-function Sheet(canvas, height, width) {
-  window.console.log(
-      'Making sheet on a canvas ' + canvas + ', sheet size: (' +
-      width + ', ' + height + ')');
+function Sheet(canvas, opts) {
+  var width = opts.width;
+  var height = opts.height;
+  var topLeft = opts.topLeft;
+
+  // Create a Paper.js Path to draw a line into it:
+  var rectangle = new paper.Rectangle(topLeft, new paper.Size(width, height));
+  var path = new paper.Path.Rectangle(rectangle);
+  // Give the stroke a color
+  path.strokeColor = 'black';
 }
